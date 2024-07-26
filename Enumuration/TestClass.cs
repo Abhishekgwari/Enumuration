9ab7e3d815d6d3119314889e14da60ed2e28fc44
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-26 20:48:05 +0530

    Add project files.

---
+++ Enumuration/TestClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumuration
+{
+    
+        public enum Days 
+        {
+            Monday=1,Tuesday=11 , Wednesday=22, Thursday=33,Friday=44
+
+        }
+     class TestClass
+    {
+        public static Days MeetingDate
+        {
+            get; set;
+        } = Days.Monday;
+        static void Main()
+        {
+            Console.WriteLine(MeetingDate);
+
+            MeetingDate = Days.Friday;
+            Console.WriteLine(MeetingDate);
+            Console.ReadLine();
+        }
+    }
+}
